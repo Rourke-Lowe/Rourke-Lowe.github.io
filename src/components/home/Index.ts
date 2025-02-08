0ee6efa6932d3493741872a622398a122c071fbd
Rourke Lowe <lowe.rourke@gmail.com>
2025-02-08 13:47:20 -0500

    edit index remove achievements

--- src/components/home/Index.ts
+++ src/components/home/Index.ts
@@ -1,3 +1,2 @@
 export { default as Hero } from './Hero/Hero';
-export { default as ExperienceSection } from './Experience/ExperienceSection';
-export { default as AchievementsSection } from './Achievements/AchievementsSection';
+export { default as ExperienceSection } from './Experience/ExperienceSection'
