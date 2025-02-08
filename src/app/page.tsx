430b0dbdad92d2e133614d76c7974ee4dfe065c6
Rourke Lowe <lowe.rourke@gmail.com>
2025-02-08 00:21:52 -0500

    test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           <>
           <Hero 
             headline="A generalist with a focus on product."
-            subtitle="My approach to product, and life, is centered around listening and being creative. For me, listening comes from a curiosity to understand what makes people tick, and creativity comes from being open. Both benefit from lots of practice."
+            subtitle="Mytest testingapproach to product, and life, is centered around listening and being creative. For me, listening comes from a curiosity to understand what makes people tick, and creativity comes from being open. Both benefit from lots of practice."
           />
             <ExperienceSection />
           </>
